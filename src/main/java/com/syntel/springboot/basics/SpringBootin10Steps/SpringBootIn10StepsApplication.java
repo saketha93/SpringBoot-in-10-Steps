38aaf81fb5f304be6e52129c9150ecfe551a40d7
Manoj kumar Lekkalapudi <manojkumarlekkalapudi@Manojs-MacBook-Pro.local>
2018-01-22 18:43:12 -0500

    Project Structure commit

---
+++ src/main/java/com/syntel/springboot/basics/SpringBootin10Steps/SpringBootIn10StepsApplication.java
@@ -0,0 +1,12 @@
+package com.syntel.springboot.basics.SpringBootin10Steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIn10StepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIn10StepsApplication.class, args);
+	}
+}
